8bcc4209d6988555283c4b9e479f5429acba2393
Praveen Kumar <praveenscience@gmail.com>
2021-08-21 06:21:10 +0100

    Controlled components and activate send.
    * Converted the textarea into controlled component by connecting to state.
    * Send button will be activated only when there are at least 3 characters.

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Compose = () => {
+  const [Message, setMessage] = useState("");
   return (
     <div className="Compose d-flex flex-row">
       <textarea
         className="flex-grow-1 compose-text"
         placeholder="Start typing your message..."
+        value={Message}
+        onChange={e => setMessage(e.target.value)}
       />
       <div className="action-buttons">
-        <button className="d-block form-control btn btn-primary btn-sm mb-1">
+        <button
+          className="d-block form-control btn btn-primary btn-sm mb-1"
+          disabled={Message.trim().length < 4}
+        >
           Send
         </button>
         <button className="d-block form-control btn btn-danger btn-sm">

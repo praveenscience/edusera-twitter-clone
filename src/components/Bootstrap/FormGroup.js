70ef5ec7495dfd9cf7da19b08a4efe52b8070669
Praveen Kumar <praveenscience@gmail.com>
2021-08-21 05:57:52 +0100

    Created an inline form group.

---
+++ src/components/Bootstrap/FormGroup.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const FormGroup = ({
+  Id,
+  Label,
+  Type,
+  onChange,
+  Value,
+  defaultValue,
+  Checked,
+  defaultChecked,
+  className,
+  Desc
+}) => {
+  return (
+    <div className={className ? className : "form-group"}>
+      <label htmlFor={Id} className="visually-hidden">
+        {Label}
+      </label>
+      <input
+        type={Type}
+        className="form-control"
+        id={Id}
+        name={Id}
+        onChange={onChange}
+        value={Value}
+        defaultValue={defaultValue}
+        checked={Checked}
+        defaultChecked={defaultChecked}
+        aria-describedby={Id + "Help"}
+      />
+      {Desc && (
+        <small id={Id + "Help"} className="form-text text-muted">
+          {Desc}
+        </small>
+      )}
+    </div>
+  );
+};
+
+export default FormGroup;

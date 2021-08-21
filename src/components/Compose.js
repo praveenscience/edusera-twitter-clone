b30f526e604606e2211a92608e0278366c49da50
Praveen Kumar <praveenscience@gmail.com>
2021-08-21 06:30:43 +0100

    Updated the sendMessage and clear message.

--- src/components/Compose.js
+++ src/components/Compose.js
@@ -1,27 +1,46 @@
 import React, { useState } from "react";
 
-const Compose = () => {
+const Compose = ({ sendMessage }) => {
   const [Message, setMessage] = useState("");
+  const handleSubmit = e => {
+    e.preventDefault();
+    sendMessage(Message);
+    setMessage("");
+  };
   return (
-    <div className="Compose d-flex flex-row">
+    <form className="Compose d-flex flex-row" onSubmit={handleSubmit}>
       <textarea
         className="flex-grow-1 compose-text"
         placeholder="Start typing your message..."
         value={Message}
+        onKeyDown={e => {
+          const evt = e || window.event;
+          console.log(evt.keyCode);
+          if (evt.keyCode === 13) {
+            evt.preventDefault();
+            if (Message.trim().length > 3) {
+              handleSubmit(e);
+            }
+          }
+        }}
         onChange={e => setMessage(e.target.value)}
       />
       <div className="action-buttons">
         <button
+          type="submit"
           className="d-block form-control btn btn-primary btn-sm mb-1"
           disabled={Message.trim().length < 4}
         >
           Send
         </button>
-        <button className="d-block form-control btn btn-danger btn-sm">
+        <button
+          type="button"
+          className="d-block form-control btn btn-danger btn-sm"
+        >
           Log Out
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
